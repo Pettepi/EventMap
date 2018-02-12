1f85141794c4c2ce1e7dc4b1558ef050d9018fc6
Petteri Piironen <pettepi@ety-a2123b-14.edu.wks.metropolia.fi>
2018-02-12 11:25:41 +0200

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: 'app.component.html',
+    styleUrls: ['app.component.css'],
 })
 export class AppComponent {
-  title = 'app';
+    title: string = 'My first AGM project';
+    lat: number = 60.169832654;
+    lng: number = 24.938162914;
+    zoom: number = 13;
 }
