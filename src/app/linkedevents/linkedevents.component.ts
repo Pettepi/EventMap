3749dc9044a77a3c77ff4477e6e8f53112f7a3b2
Petteri Piironen <pettepi@ety-a2123b-14.edu.wks.metropolia.fi>
2018-02-15 13:41:44 +0200

    linkedeventsupdate

--- src/app/linkedevents/linkedevents.component.ts
+++ src/app/linkedevents/linkedevents.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
+import {Event} from '../models/event';
 
 @Component({
     selector: 'app-linkedevents',
@@ -31,9 +32,9 @@ export class LinkedeventsComponent implements OnInit {
             file: any;
         }
 
-        this.http.get(this.apiosoite + '/event/?start=today').subscribe((data) => {
-            console.log(data);
-            this.apitulokset = data.data;
+        this.http.get<Event>(this.apiosoite + '/event/?start=today').subscribe((response) => {
+            console.log(response);
+            this.apitulokset = response.data;
         });
     }
 
